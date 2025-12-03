a96563f6bfb998942403f83a923d9616d3f34b9c
Salemofgod <fannellegnan@gmail.com>
2025-12-04 01:08:46 +0300

    Create const.js

---
+++ src/src/mock/src/const.js
@@ -0,0 +1,16 @@
+// src/const.js
+export const StatusNames = {
+  backlog: 'En Attente',
+  processing: 'En Cours',
+  done: 'Terminées',
+  trash: 'Corbeille'
+};
+
+export const StatusClasses = {
+  backlog: 'pending',
+  processing: 'active',
+  done: 'completed',
+  trash: 'deleted'
+};
+
+export const StatusOrder = ['backlog', 'processing', 'done', 'trash'];
