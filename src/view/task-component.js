02e7410f81012ef6ffa0929b7b3ebe1ee9fb6e6d
Salemofgod <fannellegnan@gmail.com>
2025-12-04 01:13:59 +0300

    Update task-component.js

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,21 +1,26 @@
+// src/view/task-component.js
 import { createElement } from '../framework/render.js';
+import { StatusClasses } from '../const.js';
 
-function createTaskComponentTemplate(taskText) {
-  return (
-    `<li class="task-card">
-      ${taskText}
-    </li>`
-  );
+function createTaskComponentTemplate(task) {
+  const { title, status } = task;
+  const statusClass = StatusClasses[status] || '';
+  
+  return `
+    <li class="task-card task--${statusClass}">
+      ${title}
+    </li>
+  `;
 }
 
 export default class TaskComponent {
-  constructor(taskText) {
-    this.taskText = taskText;
+  constructor({ task }) {
+    this.task = task;
     this.element = null;
   }
 
   getTemplate() {
-    return createTaskComponentTemplate(this.taskText);
+    return createTaskComponentTemplate(this.task);
   }
 
   getElement() {
