4fc14386fb9ace3b49d5b277be5ed2264355764f
Salemofgod <fannellegnan@gmail.com>
2025-12-03 23:39:07 +0300

    Create task.js

---
+++ src/src/mock/task.js
@@ -0,0 +1,63 @@
+// src/mock/task.js
+export const tasks = [
+  {
+    id: "1",
+    title: "Apprendre HTML",
+    status: "backlog",
+  },
+  {
+    id: "2",
+    title: "Pratiquer CSS",
+    status: "backlog",
+  },
+  {
+    id: "3",
+    title: "Comprendre Git",
+    status: "backlog",
+  },
+  {
+    id: "4",
+    title: "Lire la documentation JavaScript",
+    status: "processing",
+  },
+  {
+    id: "5",
+    title: "Construire un mini-projet",
+    status: "processing",
+  },
+  {
+    id: "6",
+    title: "Installer VS Code",
+    status: "done",
+  },
+  {
+    id: "7",
+    title: "Configurer GitHub",
+    status: "done",
+  },
+  {
+    id: "8",
+    title: "Regarder Netflix",
+    status: "trash",
+  },
+  {
+    id: "9",
+    title: "Scroller Instagram",
+    status: "trash",
+  },
+  {
+    id: "10",
+    title: "Réviser les algorithmes",
+    status: "backlog",
+  },
+  {
+    id: "11",
+    title: "Créer un portfolio",
+    status: "processing",
+  },
+  {
+    id: "12",
+    title: "Apprendre React",
+    status: "done",
+  }
+];
